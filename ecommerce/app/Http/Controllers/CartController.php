62759be6009816813d7237c143ed4385cdfbeefd
Nemanja2016 <Nemanja2016@nemanja>
2019-10-28 19:56:10 +0100

    Add method forupdate Cart action to CartController.

--- ecommerce/app/Http/Controllers/CartController.php
+++ ecommerce/app/Http/Controllers/CartController.php
@@ -21,4 +21,19 @@ class CartController extends Controller
     	Cart::add($id, $product->product_name, 1, $product->product_price, 550, ['img'=>$product->image, "stock" => $product->stock]);
     	return back();
     }
+    
+    // Update Cart Item:
+    public function update(Request $request, $id){
+        $qty = $request->qty;
+        $product_id = $request->proId;
+        $product = Product::findOrFail($product_id);
+        $stock = $product->stock;
+        
+        if ($qty < $stock) {
+            Cart::update($id, $request->qty);
+            return back()->with('status', 'Cart is updated');
+        }else{
+            return back()->with('status', 'Please check your qty is more than product stock');
+        }
+    }
 }
