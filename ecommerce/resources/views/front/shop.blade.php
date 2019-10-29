7bdb5af11ad294312999722790be576faf9ff532
Nemanja2016 <Nemanja2016@nemanja>
2019-10-29 01:26:10 +0100

    Change head and remove unnecessary description text in shop page.

--- ecommerce/resources/views/front/shop.blade.php
+++ ecommerce/resources/views/front/shop.blade.php
@@ -5,11 +5,8 @@
 <main role="main">
 
 <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <h1 class="display-4">Shop List</h1>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 </div>
 
 <div class="album py-5 bg-light">
